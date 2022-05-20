c20df02144229dc0d0ae0cb4718934ebbda48fc1
Rik Buurman <95089288+RikBuurman@users.noreply.github.com>
2022-05-20 12:52:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print('  	                  Gemaakt door Tygo de Wijn en Rik Buurman')
+print('Welkom bij Galgje!')
+print('  Laten we beginnen:')
+uitleg = input('Wil je uitleg van het spel Galgje? JA / NEE')
+if uitleg == 'JA':
+  print('test')
