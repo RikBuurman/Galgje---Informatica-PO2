ed78061ad1cab34767dc3dfe6c6cf5df6e209965
Rik Buurman <95089288+RikBuurman@users.noreply.github.com>
2022-06-04 10:47:28 +0000

    Random en time toegevoegd, random gebruikt om een woord uit de lijst te laten kiezen, sleep toegevoegd dmv time

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
+import time
+import random
 
 print('\t\t\tGemaakt door Tygo de Wijn en Rik Buurman\n\n\n')
 print('Welkom bij Galgje!')
 print('  Veel Speelplezier!')
 
-Woordenlijst = ['informatiekunde','informatica','spelletje','aardigheidje','scholier','fotografie','waardebepaling','specialiteit','verzekering','universiteit','heesterperk']
- 
-               
-uitleg = input('\nWil je uitleg van het spel Galgje? JA / NEE\n')
+woordenlijst = ['informatiekunde','informatica','spelletje','aardigheidje','scholier','fotografie','waardebepaling','specialiteit','verzekering','universiteit','heesterperk']
+
+time.sleep(1)
+             
+uitleg = input('\nWil je een uitleg over dit spel? JA / NEE\n') #wil je uitleg?
 if uitleg == 'JA':
-  print('\nOké laten we beginnen! \n De bedoleing is dat je een woord moet raden met alle letters uit het alfabet, als de letter \n in het woord voor komt en als de letter op de goede plek staat dan mag je opnieuw proberen \nen als de letter er niet in zit krijg je 5 kansen tot dat je dood bent ')
+  print('\nOké, hierbij  de uileg:\n De bedoeling is dat je een woord moet raden met alle letters uit het alfabet, als de letter \n in het woord voor komt dan wordt de letter op de goede plek gezet en dan mag je opnieuw proberen \nen als de letter er niet in zit krijg je 5 kansen tot dat je dood bent ')
 
 if uitleg == 'NEE':
-  print('\n Fijn om te horen dat je goed bent voorbereidt, laten we beginnen met het spel! \n')
+  print('\n Fijn om te horen dat je goed bent voorbereid, laten we beginnen met het spel! \n')
 
-gebruikersnaam = input('\n Vul hier je gebruikersnaam in:\n')
-print('\nGoed om je te zien,', gebruikersnaam)
+gebruikersnaam = input('\n Vul hier je gebruikersnaam in:\n') #vul je gebruikersnaam in
+print('\nGoed om je te zien,', gebruikersnaam) 
 
 spel = input('Wil je een nieuw spel beginnen? JA / NEE\n')
 if spel == 'JA':
-##start spel
-  print('*start spel*')
+  kiezen = random.choice(woordenlijst) # Kiezen is het random gekozen woord door de computer
+  
+  
 
 if spel == 'NEE':
   print('\nTot de volgende keer!')
   quit()
-
-
